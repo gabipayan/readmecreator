b9a2cce8eaa9617abf1b5c4863e1d5ab249e7035
gabipayan <gapayan26@gmail.com>
2025-03-13 20:29:06 -0600

    generateMark

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -19,7 +19,7 @@ function renderLicenseLink(license) {
   return `https://choosealicense.com/licenses/${license.toLowerCase()}/`;
 }
 // TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+// If there is no license, return an empty string    
 function renderLicenseSection(license) {
   if (!license) {
     return ''; // No añade nada si no hay licencia
